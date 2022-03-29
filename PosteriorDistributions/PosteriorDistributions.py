d2a4a06ba6f0d77a4c4c4b42301608ab1e65c5d4
Collin Stratton <Collin.Stratton00@gmail.com>
2022-03-29 15:26:46 -0700

    got previous markov stuff working with new dataset

--- PosteriorDistributions/PosteriorDistributions.py
+++ PosteriorDistributions/PosteriorDistributions.py
@@ -5,19 +5,34 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 # Import data
-df = pd.read_csv("./syntheticdata.csv", header=[0], )
+df = pd.read_csv("./syntheticdata.csv", header=None)
 print(df)
+
+# row and column headers list
+headers = ["Museum", "Concert", "Sports Event", "Restaurant", "Hike"]
 
 # Convert into a probabilistic matrix, and then convert the probabilistic matrix into a transition matrix
 # NOTE: See if we could do this automatically, without having to manually input data. Let's just use this to verify our result
-transitionMatrix = np.matrix(
-    [[1/15, 1/5, 1/3, 1/5, 1/5],
-    [7/21, 1/21, 4/21, 6/21, 3/21],
-    [8/21, 2/21, 2/21, 5/21, 4/21],
-    [5/18, 4/18, 5/18, 3/18, 1/18],
-    [2/13, 4/13, 1/13, 5/13, 1/13]]
-                             )
+transitionMatrix = df.div(df.sum(axis=1), axis=0)
 
-print(transitionMatrix)
+transitionMatrixNP = transitionMatrix.to_numpy()
 
+# function to iterate over a matrix and plot the values
+def markovPlot(matrix, x, y, n):
+    xval = np.linspace(0, 100, 100)
+    yval = []
+    for k in range(1, n):
+        yval.append(pow(matrix, k)[x, y])
+
+    plt.plot(xval, yval)
+    plt.title("{} to {}".format(headers[x], headers[y]))
+    plt.show()
+
+
+# for i, j in transitionMatrix.iterrows():
+#     markovPlot(transitionMatrixNP, i, j, 100)
+
+for i in range(transitionMatrixNP.shape[0]):
+    for j in range(transitionMatrixNP.shape[1]):
+        markovPlot(transitionMatrixNP, i, j, 101)
 
