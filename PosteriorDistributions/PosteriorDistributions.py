9463bce0a10b8e86a904b43c48e5f191b9d50587
Collin Stratton <Collin.Stratton00@gmail.com>
2022-04-02 18:22:06 -0700

    finished steps 7 and 8, worked on steps 9 and 10

--- PosteriorDistributions/PosteriorDistributions.py
+++ PosteriorDistributions/PosteriorDistributions.py
@@ -3,16 +3,16 @@ import pandas as pd
 import random as rd
 import matplotlib.pyplot as plt
 import networkx as nx
+import pymc3 as pm
 
 # Import data
 df = pd.read_csv("./syntheticdata.csv", header=None)
-print(df)
+# print(df)
 
 # row and column headers list
 headers = ["Museum", "Concert", "Sports Event", "Restaurant", "Hike"]
 
 # Convert into a probabilistic matrix, and then convert the probabilistic matrix into a transition matrix
-# NOTE: See if we could do this automatically, without having to manually input data. Let's just use this to verify our result
 transitionMatrix = df.div(df.sum(axis=1), axis=0)
 
 transitionMatrixNP = transitionMatrix.to_numpy()
@@ -48,21 +48,64 @@ def markovPlot(matrix, x, y, n):
 #     for j in range(transitionMatrixNP.shape[1]):
 #         markovPlot(transitionMatrixNP, i, j, 11)
 
-
+# Step 7:
 concertHikeRestaurant = transitionMatrixNP[1][4] * transitionMatrixNP[4][3]
-print("Percent odds of Concert to Hike to Restaurant = ", concertHikeRestaurant * 100)
+# print("Percent odds of Concert to Hike to Restaurant = ", concertHikeRestaurant * 100)
 
-trans5 = pow(transitionMatrixNP, 5)
-sum_arr = trans5.sum(axis=0)
+# Step 8:
+# find the probabilities of the transition matrix on the 5th step
+transDot = transitionMatrix
+for i in range(4):  # Four transitions, so run 4 times
+    transDot = np.dot(transDot, transitionMatrix)
 
-museum5 = sum_arr[0]
-concert5 = sum_arr[1]
-sportsEvent5 = sum_arr[2]
-restaurant5 = sum_arr[3]
-hike5 = sum_arr[4]
+# starting probability vector
+spv = np.array([.2, .2, .2, .2, .2])
 
-print("\nPercent odds of visiting the Museum 5th = ", museum5 * 100)
-print("Percent odds of visiting the Concert 5th = ", concert5 * 100)
-print("Percent odds of visiting the Sports Event 5th = ", sportsEvent5 * 100)
-print("Percent odds of visiting the Restaurant 5th = ", restaurant5 * 100)
-print("Percent odds of visiting the Hike 5th = ", hike5 * 100)
+# sum the columns of the fifth step
+sum_trans = transDot.sum(axis=0)
+
+# multiply the starting probability vector by the sum of the columns
+spv_tran = sum_trans * spv
+
+# print the likelihood of visiting any of the locations as the fifth step
+# print("\nPercent odds of visiting the Museum 5th = ", spv_tran[0] * 100)
+# print("Percent odds of visiting the Concert 5th = ", spv_tran[1] * 100)
+# print("Percent odds of visiting the Sports Event 5th = ", spv_tran[2] * 100)
+# print("Percent odds of visiting the Restaurant 5th = ", spv_tran[3] * 100)
+# print("Percent odds of visiting the Hike 5th = ", spv_tran[4] * 100)
+#
+# plt.pie(spv_tran, labels=headers, autopct='%1.1f%%')
+# plt.show()
+
+# Step 9: Format a question that requires a MCMC to answer
+# Question: What is the most visited location in town after 5 days?
+
+# Step 10:
+
+# METROPOLIS-HASTINGS
+# Define the model
+# with pm.Model() as model:
+#     # Define the prior
+#     # Prior is a matrix of probabilities
+#     x = pm.Normal('x', mu=0, sigma=1)
+#
+# with model:
+#     # Define beta and alpha
+#     b = pm.Beta('b', alpha=1, beta=1, shape=(5, 5))
+#
+# xmin = -3
+# xmax = 3
+# pmax = 0.8
+# N_MC = 100000
+#
+# t = np.random.normal(xmin, xmax, N_MC)  # get uniform temporary x values
+# y = np.random.normal(0, pmax, N_MC)  # get uniform random y values
+# # plot all the t-y pairs
+# plt.scatter(t, y, s=0.1, c='orange')
+# #make a mask that keeps index to the accepted pairs. Plot them
+# mask = y < model
+# plt.scatter(t[mask], y[mask], s=0.1)
+# accept_prob = t[mask].size / t.size
+# #histogram the t values with and without the mask
+# _ = plt.hist(t, bins=100)
+# _ = plt.hist(t[mask], bins=100)
