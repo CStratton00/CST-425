b2ea4485c49b96c42c031d730ab2db0d139236a7
drewesch <wheels.esch@gmail.com>
2022-04-14 11:53:46 -0700

    Grrrr

--- MachineLearning/dogORfood.py
+++ MachineLearning/dogORfood.py
@@ -57,7 +57,7 @@ image_dir = Path('C:/Users/Drew/OneDrive/Pictures/ml-food/images')
 images_paths = list(image_dir.glob(r'**/*.jpg'))
 labels = list(map(lambda x: os.path.split(os.path.split(x)[0])[1], images_paths))
 
-images_paths = pd.Series(images_paths, name = 'Image_Path').astype(str)
+images_paths = pd.Series(images_paths, name='Image_Path').astype(str)
 labels = pd.Series(labels, name='Label')
 
 images = pd.concat([images_paths, labels], axis=1)
@@ -65,66 +65,66 @@ images = pd.concat([images_paths, labels], axis=1)
 category_samples = []
 for category in images['Label'].unique():
     category_slice = images.query('Label == @category')
-    category_samples.append(category_slice.sample(300, random_state = 1))
+    category_samples.append(category_slice.sample(300, random_state=1))
 
 # We group in table and check the number of images by category
-images_samples = pd.concat(category_samples, axis = 0)
+images_samples = pd.concat(category_samples, axis=0)
 images_samples['Label'].value_counts()
 
 # Testing and training
-train_data, test_data = train_test_split(images_samples, train_size = 0.7, shuffle = True, random_state = 1)
+train_data, test_data = train_test_split(images_samples, train_size=0.7, shuffle=True, random_state=1)
 
 train_generator = tf.keras.preprocessing.image.ImageDataGenerator(
-    preprocessing_function = tf.keras.applications.mobilenet_v2.preprocess_input,
-    validation_split = 0.2
+    preprocessing_function=tf.keras.applications.mobilenet_v2.preprocess_input,
+    validation_split=0.2
 )
 
 test_generator = tf.keras.preprocessing.image.ImageDataGenerator(
-    preprocessing_function = tf.keras.applications.mobilenet_v2.preprocess_input
+    preprocessing_function=tf.keras.applications.mobilenet_v2.preprocess_input
 )
 
 train_images = train_generator.flow_from_dataframe(
-    dataframe = train_data,
-    x_col = 'Image_Path',
-    y_col = 'Label',
-    target_size = (224, 224),
-    color_mode = 'rgb',
-    class_mode = 'categorical',
-    batch_size = 32,
-    shuffle = True,
-    seed = 42,
-    subset = 'training'
+    dataframe=train_data,
+    x_col='Image_Path',
+    y_col='Label',
+    target_size=(224, 224),
+    color_mode='rgb',
+    class_mode='categorical',
+    batch_size=32,
+    shuffle=True,
+    seed=42,
+    subset='training'
 )
 
 val_images = train_generator.flow_from_dataframe(
-    dataframe = train_data,
-    x_col = 'Image_Path',
-    y_col = 'Label',
-    target_size = (224, 224),
-    color_mode = 'rgb',
-    class_mode = 'categorical',
-    batch_size = 32,
-    shuffle = True,
-    seed = 42,
-    subset = 'training'
+    dataframe=train_data,
+    x_col='Image_Path',
+    y_col='Label',
+    target_size=(224, 224),
+    color_mode='rgb',
+    class_mode='categorical',
+    batch_size=32,
+    shuffle=True,
+    seed=42,
+    subset='training'
 )
 
 test_images = test_generator.flow_from_dataframe(
-    dataframe = test_data,
-    x_col = 'Image_Path',
-    y_col = 'Label',
-    target_size = (224, 224),
-    color_mode = 'rgb',
-    class_mode = 'categorical',
-    batch_size = 32,
-    shuffle = False
+    dataframe=test_data,
+    x_col='Image_Path',
+    y_col='Label',
+    target_size=(224, 224),
+    color_mode='rgb',
+    class_mode='categorical',
+    batch_size=32,
+    shuffle=False
 )
 
 pretrained_model = tf.keras.applications.MobileNetV2(
-    input_shape = (224, 224, 3),
-    include_top = False, #quitamos la ultima capa del modelo original
-    weights = 'imagenet',
-    pooling = 'avg',
+    input_shape=(224, 224, 3),
+    include_top=False,  # quitamos la ultima capa del modelo original
+    weights='imagenet',
+    pooling='avg',
 )
 
 # Freeze the weights of the network to maintain transfer learning
@@ -133,13 +133,45 @@ pretrained_model.trainable = False
 inputs = pretrained_model.input
 
 # We create two layers of our own
-x = tf.keras.layers.Dense(128, activation = 'relu')(pretrained_model.output)
-x = tf.keras.layers.Dense(128, activation = 'relu')(x)
+x = tf.keras.layers.Dense(128, activation='relu')(pretrained_model.output)
+x = tf.keras.layers.Dense(128, activation='relu')(x)
 
 # We create our output layer, with the 101 classes we have
-output_layer = tf.keras.layers.Dense(101, activation = 'softmax')(x)
+output_layer = tf.keras.layers.Dense(101, activation='softmax')(x)
 
 # We unite the original model with our new layers
 model = tf.keras.Model(inputs, output_layer)
 
 print(model.summary())
+
+# Training
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+history = model.fit(
+    train_images,
+    validation_data=val_images,
+    epochs=50,
+    callbacks=[
+        tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+    ]
+)
+
+# Training Results - Prediction Accuracy
+results = model.evaluate(test_images, verbose=1)
+print("Precision: {:.2f}%".format(results[1] * 100))
+
+# Making a prediction with an internet image
+import requests
+from PIL import Image
+from io import BytesIO
+import cv2
+
+url = 'https://www.hola.com/imagenes/cocina/tecnicas-de-cocina/20210820194795/como-hacer-patatas-fritas-perfectas/0-986-565/portada-patatas-age-t.jpg'
+response = requests.get(url)
+img = Image.open(BytesIO(response.content))
+img = np.array(img).astype(float) / 255
+
+img = cv2.resize(img, (224, 224))
+predict = model.predict(img.reshape(-1, 224, 224, 3))
+
+print(np.argmax(predict))
