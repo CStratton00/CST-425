8474f22ff9d9f615d1ae1d793ae92a62ec2464f7
EvanLee25 <evan.lee25@outlook.com>
2022-09-23 14:33:30 -0700

    semi finished spaceship mlp

--- 435-Space-KNN-Kmeans/spaceship-titanic.py
+++ 435-Space-KNN-Kmeans/spaceship-titanic.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-%matplotlib inline
+#%matplotlib inline
 warnings.filterwarnings('ignore')
 
 
@@ -16,16 +16,20 @@ train.shape  # shape gives us the number of rows and columns
 ##################################
 train.info()
 ##################################
-train.isnull().sum()
+print(train.isnull().sum())
 ##################################
+# Test comparisons
+print(f"Amount of transported people = {train['Transported'].value_counts()}")
 f, ax = plt.subplots(1, 2, figsize=(12, 4))
-train['Survived'].value_counts().plot.pie(
+train['Transported'].value_counts().plot.pie(
     explode=[0, 0.1], autopct='%1.1f%%', ax=ax[0], shadow=False)
 ax[0].set_title('Survivors (1) and the dead (0)')
 ax[0].set_ylabel('')
-sns.countplot('Survived', data=train, ax=ax[1])
+sns.countplot('Transported', data=train, ax=ax[1])
 ax[1].set_ylabel('Quantity')
 ax[1].set_title('Survivors (1) and the dead (0)')
 plt.show()
+
+
 ##################################
 
